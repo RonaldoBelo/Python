530ca43addd94aa18cfd616a943314adeb92b4cc
RonaldoBelo <162352657+RonaldoBelo@users.noreply.github.com>
2024-09-16 16:40:49 -0300

    Add files via upload

---
+++ metodos.py
@@ -0,0 +1,10 @@
+numeros1 = [1, 2, 3, 4, 5]
+numeros1.append(10)
+print(numeros1)
+
+numeros2 = [1, 2, 3, 4, 5]
+numeros2.insert(0, 9)
+print(numeros2)
+print(3 in numeros2)
+print(len(numeros2))
+print(numeros2.clear())
