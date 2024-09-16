c8499cd24c27499498af50ffcd7e4cff2f6c3736
RonaldoBelo <162352657+RonaldoBelo@users.noreply.github.com>
2024-09-16 16:40:07 -0300

    Add files via upload

---
+++ oper.py
@@ -0,0 +1,16 @@
+a = int(input("Digite um numero: "))
+b = int(input("Digite outro numero: "))
+soma = a + b
+sub = a - b
+mult = a * b
+div = a / b
+div_int = a // b
+rest = a % b
+exp = a ** b
+print(f'Soma: {soma}')
+print(f'Subtração: {sub}')
+print(f'Multiplicação: {mult}')
+print(f'Divisão: {div}')
+print(f'Divisão Inteira: {div_int}')
+print(f'Resto da Divisão: {rest}')
+print(f'Exponenciação: {exp}')
