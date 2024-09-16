82660bf04fa7f6320bc69f1a0ba3b13984795caa
RonaldoBelo <162352657+RonaldoBelo@users.noreply.github.com>
2024-09-16 16:40:15 -0300

    Add files via upload

---
+++ condicional.py
@@ -0,0 +1,8 @@
+temp = 25
+if temp > 30:
+    print("Hoje o dia esta quente...")
+    print("Beba muita água!")
+elif temp > 20:
+    print("Hoje o clima está agradável!")
+else:
+    print("Hoje está frio!")
